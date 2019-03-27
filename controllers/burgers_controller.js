52f2b91cde98c75b66bc80220ca54eab0b6f1b26
adventuroussheep <jacob_haas_7@hotmail.com>
2019-03-26 17:45:37 -0700

    added jawsDB

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burgers.js");
 
+
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {
